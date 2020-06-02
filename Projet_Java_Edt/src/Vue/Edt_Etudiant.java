5ba73135585fa31c2b99eab711fde7088dd88f38
wangdavidhao <wangdavid1999@gmail.com>
2020-06-02 15:42:24 +0200

    vue modifiéé

--- Projet_Java_Edt/src/Vue/Edt_Etudiant.java
+++ Projet_Java_Edt/src/Vue/Edt_Etudiant.java
@@ -273,8 +273,7 @@ public class Edt_Etudiant extends Edt {
             JButton lancerrecherche = new JButton(new AbstractAction("Rechercher") {
                 @Override
                 public void actionPerformed(ActionEvent actionEvent) {
-                    control_recherche = new RechercheControleur();
-                    control_recherche.rechercher_utilisateur(nom.getText(), semaine.getText(), 4);
+                    rechercher_utilisateur(nom.getText(), semaine.getText(), 4);
                 }
             });
 
@@ -452,6 +451,81 @@ public class Edt_Etudiant extends Edt {
         }
 
 
+    }
+    public void rechercher_utilisateur(String nom, String semaine, int droit) {
+
+        int numero_semaine = 0;
+        boolean validationdacces = false;
+
+        // On verifie que l'utilisateur a bien ecrit un entier pour la semaine
+        try {
+            numero_semaine = Integer.parseInt(semaine);
+        } catch (NumberFormatException e) {
+            System.out.println("Numero de semaine non valide");
+            return;
+        }
+
+        // On crée les utilisateurs et on va faire une recherche dessus
+        UtilisateurDao userDAO = new UtilisateurDao();
+        ArrayList<Utilisateur> mesUsers = new ArrayList<>();
+
+        for (int i = 1; i < userDAO.getTaille("utilisateur") + 1; i++) {
+            mesUsers.add(userDAO.find(i));
+        }
+
+        Utilisateur leusersouhaiter = new Utilisateur();
+
+        for (int i = 0; i < mesUsers.size(); i++) {
+            if (mesUsers.get(i).getNom().equals(nom)) {
+                leusersouhaiter = new Utilisateur(mesUsers.get(i));///dés qu'on trouve le bon utilisateur on sort de la boucle
+                break;
+            }
+        }
+        for (int i = 0; i < mesUsers.size(); i++) {
+            if (mesUsers.get(i).getNom().equals(nom) && mesUsers.get(i).getDroit() == droit) {
+                leusersouhaiter = new Utilisateur(mesUsers.get(i));
+                break;
+            }
+        }
+
+        if (droit == 1 || droit == 2) {// Admin et Referent on une vue total sur les utilisateur
+            validationdacces = true;
+        } else {
+            if (droit == leusersouhaiter.getDroit())/// Pour les prof et etudiant, il ne peuvent voir que respectivement prof et etudiant
+                validationdacces = true;
+        }
+
+        if (leusersouhaiter.getID() != 0) {
+            if (validationdacces) {
+                System.out.println("Les informations de la personne sont :");
+                System.out.println(leusersouhaiter.getID() + " " + leusersouhaiter.getNom() + " " + leusersouhaiter.getPrenom()
+                        + " " + leusersouhaiter.getMail() + " " + leusersouhaiter.getDroit());
+
+                // On recupère les information de l'utilisateur si possible
+                ArrayList<Seance> lesSeances = userDAO.lesSeance(leusersouhaiter.getID(), numero_semaine);//ICI LES SEANCES
+
+                System.out.println("Son emploi du temps est le suivant :");
+                for (Seance uneseance :
+                        lesSeances) {
+                    System.out.println(uneseance.getID() + " " + uneseance.getSemaine() + " " + uneseance.getDate().toString() + " " + uneseance.getHeureDebut()
+                            + " " + uneseance.getHeureFin() + " " + uneseance.getCours().getNom() + " " + uneseance.getType().getNom());
+                }
+                System.out.println("Il faut modifier la vu pour les afficher correctement");
+            } else System.out.println("Accés non autorisé");
+        } else
+            System.out.println("Personne non trouvé dans la BDD : " + nom);
+
+    }
+
+    
+
+    
+
+    /**
+     * Renvoie un recap de toutes les informations d'un enseignant
+     */
+    public void voirrecap(Enseignant prof, java.sql.Date date_debut, java.sql.Date date_fin) {
+        System.out.println("je suis " + prof.getNom() + " je veux mon emplois du temps du " + date_debut + " au " + date_fin);
     }
 
 }
