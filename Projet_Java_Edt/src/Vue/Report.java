5c6a7906ece1504f779059f2788fea76226c04ce
noahghost <39402130+noahghost@users.noreply.github.com>
2020-06-07 18:08:01 +0200

    fichier inutile enlever

--- Projet_Java_Edt/src/Vue/Report.java
+++ Projet_Java_Edt/src/Vue/Report.java
@@ -3,8 +3,10 @@ package Vue;
 
 
 import Modele.*;
+
 import java.util.ArrayList;
 import javax.swing.*;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
@@ -13,44 +15,39 @@ import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
 /**
- *
  * @author Wang David
  */
-public class Report extends JFrame{
-    
-    public Report(){}
-    
-    public Report(Etudiant etudiant)
-    {
+public class Report extends JFrame {
+
+    public Report() {
+    }
+
+    public Report(Etudiant etudiant) {
         super("Votre reporting");
 
-        this.setSize(1000,800); //Taille
+        this.setSize(1000, 800); //Taille
         this.setLocationRelativeTo(null); //Centre
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Stop run quand la dernière fenetre est fermée
         this.setResizable(false);
-        
+
         DefaultPieDataset dataset = new DefaultPieDataset();
         CoursDao coursDao = new CoursDao();
-        
+
         EtudiantDao eDao = new EtudiantDao();
-        ArrayList<Cours> cours_e= new ArrayList();
+        ArrayList<Cours> cours_e;
         cours_e = eDao.trouverAllCours(etudiant.getID());
-        
-        if(cours_e.isEmpty())
-        {
+
+        if (cours_e.isEmpty()) {
             System.out.println("Pas de cours");
-        }
-        else
-        {
+        } else {
             for (int i = 1; i < cours_e.size(); i++) {
-            String nom_cours = cours_e.get(i).getNom();
-            int nombre_seance = coursDao.nombreDeSeance(i);
-            dataset.setValue(nom_cours, nombre_seance);
-        }
+                String nom_cours = cours_e.get(i).getNom();
+                int nombre_seance = coursDao.nombreDeSeance(i);
+                dataset.setValue(nom_cours, nombre_seance);
+            }
         }
 
-        
-
+/*
         DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
         GroupeDAO groupeDAO = new GroupeDAO();
 
@@ -59,6 +56,9 @@ public class Report extends JFrame{
             int nombre_seance = groupeDAO.nombreDeSeance(i);
             dataset1.setValue(nombre_seance, nom_groupe, "");
         }
+  */
+        DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
+        EtudiantDao etudiantDao = new EtudiantDao();
 
         DefaultPieDataset dataset2 = new DefaultPieDataset();
         SiteDAO siteDAO = new SiteDAO();
@@ -99,37 +99,32 @@ public class Report extends JFrame{
         this.add(panel);
         this.setVisible(true);
     }
-    
-    public Report(Enseignant prof)
-    {
+
+    public Report(Enseignant prof) {
         super("Votre reporting");
 
-        this.setSize(1000,800); //Taille
+        this.setSize(1000, 800); //Taille
         this.setLocationRelativeTo(null); //Centre
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Stop run quand la dernière fenetre est fermée
         this.setResizable(false);
-        
+
         DefaultPieDataset dataset = new DefaultPieDataset();
         CoursDao coursDao = new CoursDao();
         EnseignantDAO eDao = new EnseignantDAO();
         ArrayList<Cours> cours_e = new ArrayList();
-        
+
         cours_e = eDao.trouverAllCours(prof.getID());
-        
-        if(cours_e.isEmpty())
-        {
+
+        if (cours_e.isEmpty()) {
             System.out.println("Pas de cours");
-        }
-        else
-        {
+        } else {
             for (int i = 1; i < cours_e.size(); i++) {
-            String nom_cours = cours_e.get(i).getNom();
-            int nombre_seance = coursDao.nombreDeSeance(i);
-            dataset.setValue(nom_cours, nombre_seance);
-        }
+                String nom_cours = cours_e.get(i).getNom();
+                int nombre_seance = coursDao.nombreDeSeance(i);
+                dataset.setValue(nom_cours, nombre_seance);
+            }
         }
 
-        
 
         DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
         GroupeDAO groupeDAO = new GroupeDAO();
@@ -181,23 +176,20 @@ public class Report extends JFrame{
 
         this.setVisible(true);
     }
-    
-    public Report(Utilisateur admin)
-    {
+
+    public Report(Utilisateur admin) {
         super("Votre reporting");
 
-        this.setSize(1000,800); //Taille
+        this.setSize(1000, 800); //Taille
         this.setLocationRelativeTo(null); //Centre
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //Stop run quand la dernière fenetre est fermée
         this.setResizable(false);
-        
+
         DefaultPieDataset dataset = new DefaultPieDataset();
         CoursDao coursDao = new CoursDao();
         EnseignantDAO eDao = new EnseignantDAO();
         ArrayList<Cours> cours_e = new ArrayList();
-        
-        
-        
+
 
         DefaultCategoryDataset dataset1 = new DefaultCategoryDataset();
         GroupeDAO groupeDAO = new GroupeDAO();
@@ -236,16 +228,11 @@ public class Report extends JFrame{
         );
 
 
- 
         ChartPanel panel2 = new ChartPanel(chart1);
 
         this.add(panel2);
         this.setVisible(true);
     }
-            
-    
-    
-    
-    
-    
+
+
 }
